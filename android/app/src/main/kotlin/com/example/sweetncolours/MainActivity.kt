9f5e312f1d4a9cd67cf54151a96f8b2bf7e2f193
lloyd031 <elwiennepetiluna@gmail.com>
2023-05-08 17:41:19 +0800

    first

--- android/app/src/main/kotlin/com/example/sweetncolours/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.sweetncolours
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\src\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\elwie\\OneDrive\\Documents\\App Dev\\sweetncolours" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "PROJECT_DIR=C:\\Users\\elwie\\OneDrive\\Documents\\App Dev\\sweetncolours"
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\elwie\\OneDrive\\Documents\\App Dev\\sweetncolours\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\elwie\\OneDrive\\Documents\\App Dev\\sweetncolours"
+  "FLUTTER_TARGET=C:\\Users\\elwie\\OneDrive\\Documents\\App Dev\\sweetncolours\\lib\\main.dart"
+  "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz1mYWxzZQ==,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC8yM2Y3MzBlZmJmZjcyM2Y1NzdmYjllNTljYTNkODhmN2VjMDExZTc2Lw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\elwie\\OneDrive\\Documents\\App Dev\\sweetncolours\\.dart_tool\\package_config.json"
+)
